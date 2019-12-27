49fa32691dbbca109b1de40a349037e97169859d
Vinod Madubashana <vinod.madubashana@cloudsolution-sa.com>
2019-12-27 12:12:56 +0530

    enable idempotent producer in twitter producer

--- src/main/java/com/vinod/kafka/advance/TwitterProducer.java
+++ src/main/java/com/vinod/kafka/advance/TwitterProducer.java
@@ -82,6 +82,13 @@ public class TwitterProducer {
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
+        //safe producer
+        properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
+        properties.setProperty(ProducerConfig.ACKS_CONFIG, "all");
+        properties.setProperty(ProducerConfig.RETRIES_CONFIG, Integer.toString(Integer.MAX_VALUE));
+        properties.setProperty(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "5");
+
+
         // Create the Producer
         return new KafkaProducer<String, String>(properties);
     }
